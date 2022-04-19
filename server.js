33f67c24448e848eb620769c15912cf701de2868
mezzy677 <mezzy677@gmail.com>
2022-04-19 12:32:41 +0530

    add node start in package.json

--- server.js
+++ server.js
@@ -10,5 +10,8 @@ const APP = EXPRESS();
 require('./config/database.config')(CONFIG[env]);
 require('./config/express')(APP);
 require('./config/routes')(APP);
-
-APP.listen(process.env.PORT || 8081);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+APP.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
