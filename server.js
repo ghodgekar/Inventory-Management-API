cc58c7a30a79a2b891208f43956c93dce20c02d1
mezzy677 <mezzy677@gmail.com>
2022-04-19 12:31:27 +0530

    add node start in package.json

--- server.js
+++ server.js
@@ -12,8 +12,3 @@ require('./config/express')(APP);
 require('./config/routes')(APP);
 
 APP.listen(process.env.PORT || 8081);
-<<<<<<< HEAD
-console.log(`Server is listening on port ${PORT}`);
-=======
-console.log(`Server is listening on port ${PORT}`);
->>>>>>> f9195f91a38fccfdcb353e1e082da0fc24dadc03
